696ef32e7358b527f68d0081969afcfddcc0f155
Nathan Alam <nathanalam99@gmail.com>
2022-11-29 21:40:04 -0500

    Add diffusion to export

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='stablediffusion',
     version='0.0.1',
     description='',
-    packages=["ldm"],
+    packages=["ldm", "ldm.models.diffusion"],
     install_requires=[
         'torch',
         'numpy',
