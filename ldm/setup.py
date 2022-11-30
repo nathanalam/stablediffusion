e192459d5b8cf72bfff228e282898877e959ed56
Nathan Alam <nathanalam99@gmail.com>
2022-11-29 21:53:38 -0500

    Update setup.py

--- ldm/setup.py
+++ ldm/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ldm',
-    version='0.0.1',
+    version='0.1.3',
     description='',
     packages=find_packages(),
     install_requires=[
