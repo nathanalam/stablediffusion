da8cdaf8d6e5128ad35ca70b19985b4ea6d3d91b
Nathan Alam <nathanalam99@gmail.com>
2022-11-29 21:37:02 -0500

    Change to define packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='stablediffusion',
     version='0.0.1',
     description='',
-    packages=find_packages(),
+    packages=["ldm"],
     install_requires=[
         'torch',
         'numpy',
